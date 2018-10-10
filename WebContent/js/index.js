f0d7537151ecb62f1371674e01803604a19c3a66
25184 <25184@fang>
2018-10-10 17:31:50 +0800

    nice

---
+++ WebContent/js/index.js
@@ -0,0 +1,10 @@
+/*var menuFlag=0;
+
+
+$("#menu").click(function(){
+  switch(menuFlag){
+  	case 0:$("#main").css("width","90%");menuFlag=1;break;
+  	case 1:$("#main").css("width","97%");menuFlag=0;break;
+  }
+   })
+*/
